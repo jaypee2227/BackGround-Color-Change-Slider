995a062790fc1b817c495e6e774168039c844c47
B V Jayprakash <62664902+jaypee2227@users.noreply.github.com>
2023-07-24 22:37:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+var red = document.querySelector("#red");
+var green = document.querySelector("#green");
+var blue = document.querySelector("#blue");
+
+var read = document.querySelector("#readText");
+
+var res = document.querySelector("#container12");
+
+var btn = document.querySelector("#btn");
+
+btn.addEventListener("click", changeColour);
+
+function changeColour() {
+  let redd = red.value;
+  let greenn = green.value;
+  let bluee = blue.value;
+
+  let result = `rgb(${redd},${greenn},${bluee})`;
+
+  res.style.backgroundColor = result;
+  read.innerHTML = result;
+}
